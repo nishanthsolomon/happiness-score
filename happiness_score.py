eb086fb604c48e3edc7161e1a9f9e061aa3c1c19
nishanthsolomon <nsolomo2@asu.edu>
2020-02-23 22:11:14 -0700

    Added graph plot code

--- happiness_score.py
+++ happiness_score.py
@@ -1,9 +1,12 @@
 import configparser
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.metrics import mean_squared_error
 from math import sqrt
+from mpl_toolkits.mplot3d import Axes3D
 
 from linear_regression import LinearRegressionHappinessScore
 from mlp import MLPHappinessScore
@@ -42,8 +45,25 @@ class HappinessScore():
         return rmse
 
     def rmse(self, y, y_predicted):
+        self.plot_results(y_predicted)
         rms = sqrt(mean_squared_error(y, y_predicted))
         return rms
+
+    def plot_results(self, y_predicted):
+        X = np.array(self.x_test)
+        Y = np.array(self.y_test)
+        Y_predicted = np.array(y_predicted)
+
+        fig = plt.figure(1)
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(X[:, 0], X[:, 1], Y)
+        ax.scatter(X[:, 0], X[:, 1], Y, color='r', label='Actual Happiness Score')
+        ax.scatter(X[:, 0], X[:, 1], Y_predicted, color='g', label='Predicted Happiness Score')
+        ax.set_xlabel('GDP')
+        ax.set_ylabel('Life Expectancy')
+        ax.set_zlabel('Happiness score')
+        ax.legend()
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -55,5 +75,5 @@ if __name__ == "__main__":
     rmse_linear_regression = happiness_score.run_linear_regression()
     rmse_mlp = happiness_score.run_mlp()
 
-    print('RMS error of linear regression model' + str(rmse_linear_regression))
-    print('RMS error of mlp model' + str(rmse_mlp))
+    print('RMS error of linear regression model = ' + str(rmse_linear_regression))
+    print('RMS error of mlp model = ' + str(rmse_mlp))
